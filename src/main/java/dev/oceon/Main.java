8238793e0c876301644445283d8373e405372fd8
emoceon@gmail.com <emoceon@gmail.com>
2024-10-08 23:16:28 +0100

    first sout

--- src/main/java/dev/oceon/Main.java
+++ src/main/java/dev/oceon/Main.java
@@ -2,6 +2,6 @@ package dev.oceon;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to the Baggage-Train D&D Management System!");
     }
 }
